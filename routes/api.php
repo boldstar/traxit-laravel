0f6b33f98753b4dee329457c73490aa4b413b2bd
Thomas <thomasweems3@gmail.com>
2018-12-21 15:26:42 -0600

    testing

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,6 @@ use App\Exports\ClientsExport;
 |
 */
 
-Route::options('{any}', function () {
-    return response('OK', \Illuminate\Http\Response::HTTP_NO_CONTENT)
-          ->header('Access-Control-Allow-Origin', implode(',', config('cors.default_profile.allow_origins')))
-          ->header('Access-Control-Allow-Methods', implode(',', config('cors.default_profile.allow_methods')))
-          ->header('Access-Control-Allow-Headers', implode(',', config('cors.default_profile.allow_headers')));
-});
-
 //auth:api requires access token to access controllers
 Route::group(['middleware' => 'auth:api'], function () {   
     Route::get('/tasks', 'Tenant\TasksController@index');
@@ -32,6 +25,14 @@ Route::group(['middleware' => 'auth:api'], function () {
 
 //tenancy.enforce is a middleware for making routes aware of tenancy
 Route::group(['middleware' => 'tenancy.enforce'], function () {
+    Route::options('{any}', function () {
+        return response('OK', \Illuminate\Http\Response::HTTP_NO_CONTENT)
+              ->header('Access-Control-Allow-Origin', implode(',', config('cors.default_profile.allow_origins')))
+              ->header('Access-Control-Allow-Methods', implode(',', config('cors.default_profile.allow_methods')))
+              ->header('Access-Control-Allow-Headers', implode(',', config('cors.default_profile.allow_headers')));
+    });
+
+    
     Route::get('/account', 'Tenant\AccountsController@account');
     Route::post('/account', 'Tenant\AccountsController@store');
     Route::patch('/account/{id}', 'Tenant\AccountsController@update');
