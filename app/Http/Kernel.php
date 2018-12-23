bb1e966ff0d3f97618bd630cf4cb660aa95a7c56
Thomas <thomasweems3@gmail.com>
2018-12-22 21:33:44 -0600

    change preflight responses

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Spatie\Cors\Cors::class,
         \App\Http\Middleware\PreflightResponse::class,
+        \Spatie\Cors\Cors::class,
     ];
 
     /**
--- app/Http/Middleware/PreflightResponses.php
+++ app/Http/Middleware/PreflightResponses.php
@@ -15,10 +15,14 @@ class PreflightResponses
      */
     public function handle($request, Closure $next)
     {
-        if ($request->getMethod() === "OPTIONS") {
-            return response('');
-        }
+        if($request->getMethod() == 'OPTIONS'){
 
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+            header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization');
+            header('Access-Control-Allow-Credentials: true');
+            exit(0);
+        }
         return $next($request);
     }
 }
