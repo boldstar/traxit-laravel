746c3aa91c8f41103fbaac3452c44d02844bbf8e
Thomas <thomasweems3@gmail.com>
2018-12-22 21:29:22 -0600

    testing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Spatie\Cors\Cors::class,
-        Middleware\PreflightResponse::class,
+        \App\Http\Middleware\PreflightResponse::class,
     ];
 
     /**
