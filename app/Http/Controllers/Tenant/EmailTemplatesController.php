7d681ebe613b4193173774e750f37df8af229325
Thomas <thomasweems3@gmail.com>
2019-03-17 13:58:34 -0500

    change html dom parser package

--- app/Http/Controllers/Tenant/EmailTemplatesController.php
+++ app/Http/Controllers/Tenant/EmailTemplatesController.php
@@ -8,7 +8,7 @@ use App\Models\Tenant\Client;
 use App\Models\Tenant\Question;
 use App\Models\Tenant\Engagement;
 use App\Models\Tenant\Account;
-use Sunra\PhpSimple\HtmlDomParser;
+use KubAT\PhpSimple\HtmlDomParser;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\StartConversation;
@@ -24,37 +24,40 @@ class EmailTemplatesController extends Controller
     public function index()
     {
         $templates = EmailTemplate::all();
-        return response('Good', 401);
-        foreach($templates as $template) {
-            $document = HtmlDomParser::str_get_html($template->html_template);
-            $document->find('i', 0)->innertext = '<strong>Contacts Name</strong>,<br><br>';
-            $document->find('span[class=year]', 0)->innertext = '<strong>Year</strong>';
-            $document->find('span[class=return]', 0)->innertext = '<strong>Return Type</strong>';
-            $document->find('span[class=name]', 0)->innertext = '<strong>Name On Return</strong>';
-            $pending = $document->find('h3[class=pending]', 0);
-            if($pending != null) {
-                $pending->innertext = '<br><br><div style="border: 1px solid blue; border-radius: 5px; padding: 10px;">Pending Questions:<br>';
+        try {
+            foreach($templates as $template) {
+                $document = HtmlDomParser::str_get_html($template->html_template);
+                $document->find('i', 0)->innertext = '<strong>Contacts Name</strong>,<br><br>';
+                $document->find('span[class=year]', 0)->innertext = '<strong>Year</strong>';
+                $document->find('span[class=return]', 0)->innertext = '<strong>Return Type</strong>';
+                $document->find('span[class=name]', 0)->innertext = '<strong>Name On Return</strong>';
+                $pending = $document->find('h3[class=pending]', 0);
+                if($pending != null) {
+                    $pending->innertext = '<br><br><div style="border: 1px solid blue; border-radius: 5px; padding: 10px;">Pending Questions:<br>';
+                }
+                $question = $document->find('p[class=question]', 0);
+                if($question != null) {
+                    $question->innertext = '<strong>List Of Questions</strong></div><br>';
+                }
+                $status = $document->find('span[class=status]', 0);
+                if($status != null) {
+                    $status->innertext = '';
+                }
+                $current = $document->find('h3[class=current]', 0);
+                if($current != null) {
+                    $current->innertext = '<br><br><div style="border: 1px solid blue; border-radius: 5px; padding: 10px;">Current Status: <strong>Current Status</strong></div><br>';
+                }
+                $document->find('span[class=phone]', 0)->innertext = '<strong>Phone Number</strong><br>';
+                $document->find('span[class=email]', 0)->innertext = '<strong>Users Email</strong><br><br>';
+                $document->find('p[class=looking]', 0)->innertext = '<p>Looking forward to hearing from you, Thanks!</p><br>';
+                $document->find('h3[class=account]', 0)->innertext = '<strong>Company Name</strong><br>';
+                $document->find('h3[class=account-email]', 0)->innertext = '<strong>Account Email</strong><br>';
+                $document->find('h3[class=phone]', 0)->innertext = '<strong>Company Phone Number</strong><br>';
+                $document->find('h3[class=fax]', 0)->innertext = '<strong>Company Fax Number</strong>';
+                $template->html_template = $document->plaintext;
             }
-            $question = $document->find('p[class=question]', 0);
-            if($question != null) {
-                $question->innertext = '<strong>List Of Questions</strong></div><br>';
-            }
-            $status = $document->find('span[class=status]', 0);
-            if($status != null) {
-                $status->innertext = '';
-            }
-            $current = $document->find('h3[class=current]', 0);
-            if($current != null) {
-                $current->innertext = '<br><br><div style="border: 1px solid blue; border-radius: 5px; padding: 10px;">Current Status: <strong>Current Status</strong></div><br>';
-            }
-            $document->find('span[class=phone]', 0)->innertext = '<strong>Phone Number</strong><br>';
-            $document->find('span[class=email]', 0)->innertext = '<strong>Users Email</strong><br><br>';
-            $document->find('p[class=thanks]', 0)->innertext = '<p>Looking forward to hearing from you, Thanks!</p><br>';
-            $document->find('h3[class=account]', 0)->innertext = '<strong>Company Name</strong><br>';
-            $document->find('h3[class=account-email]', 0)->innertext = '<strong>Account Email</strong><br>';
-            $document->find('h3[class=phone]', 0)->innertext = '<strong>Company Phone Number</strong><br>';
-            $document->find('h3[class=fax]', 0)->innertext = '<strong>Company Fax Number</strong>';
-            $template->html_template = $document->plaintext;
+        } catch(\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 422);
         }
         return response($templates);
     }
