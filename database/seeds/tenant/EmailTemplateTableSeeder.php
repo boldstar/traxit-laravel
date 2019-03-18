48b68fad462312c013f0c1a3a7daddbe2b3223f6
Thomas <thomasweems3@gmail.com>
2019-03-17 20:05:21 -0500

    fixed email again

--- database/seeds/tenant/EmailTemplateTableSeeder.php
+++ database/seeds/tenant/EmailTemplateTableSeeder.php
@@ -71,7 +71,7 @@ class EmailTemplateTableSeeder extends Seeder
         <div>
             <p style="margin: 0; align-self: center">Or reply to: <span class="email">{{ $userEmail }}</span></p>
         </div>
-        <p>Please reply to this email to verify you received it and let me know when you will be able to get me this information.</p>
+        <p>Please reply to this email to verify you received it.</p>
         <p class="looking">Looking forward to hearing from you, Thanks!</p>
         <br>
         
