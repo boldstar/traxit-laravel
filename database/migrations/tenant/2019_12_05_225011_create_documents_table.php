cf21fd89cecc64150ef5c95cf0a01e8b0f57ff83
Thomas <thomasweems3@gmail.com>
2019-12-05 19:25:03 -0600

    document model migrated

---
+++ database/migrations/tenant/2019_12_05_225011_create_documents_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->string('document_name');
+            $table->text('path');
+            $table->text('message');
+            $table->string('account');
+            $table->boolean('downloadable')->default(true);
+            $table->boolean('payment_required')->default(false);
+            $table->boolean('signature_required')->default(false);
+            $table->boolean('paid')->default(false);
+            $table->boolean('signed')->default(false);
+            $table->string('uploaded_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
