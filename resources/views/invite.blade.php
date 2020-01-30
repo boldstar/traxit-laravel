0fdd53b21c52fd16868b9305b7d5393038465c80
Thomas <thomasweems3@gmail.com>
2020-01-30 15:38:17 -0600

    updated invite email layout

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -5,39 +5,50 @@ Hello <i>{{ $client["client"]->first_name }}
         {{ $client["client"]->last_name }}, 
         </i>
 
+
+
 <div style="
-display: flex;
-align-items: center;
-text-align:left;
+text-align:center;
 margin: 25px;
 ">
-<p 
- style="
- color:black;
- ">
-You are receiving this email as an invitation to our document sharing portal.<br><br> By clicking the button below you will be asked to register with our service so that we can manage documents securely with eachother.<br><br>Once registered you will be able to sign and download documents shared by us.
-</p>
+        <div>
+        <h1 style="color:black;">{{ $accountName }}</h1>
+        </div>
+        <div>
+        <p 
+        style="
+        color:black;
+        background: rgb(243, 243, 243);
+        margin-bottom: 30px;
+        padding: 10px;
+        border-radius: 8px;
+        ">You are receiving this email as an invitation to our document sharing portal.<br> By clicking the button below you will be asked to register with our service<br> so that we can manage documents securely with eachother.<br>Once registered you will be able to sign and download documents shared by us.
+        </p>
+        </div>
+        <div>
+        <a href="http://localhost:8081/register?client_id={{$client['client']->id}}&client_email={{$client_email}}"
+        style="
+        background: #0077ff;
+        padding: 10px 40px;
+        border: none;
+        border-radius: 8px;
+        text-decoration: none;
+        margin: 20px;
+        color: white;
+        font-weight: bold;
+        font-size: 1.25rem;
+        "
+        >Click To Register</a>
+        </div>
+        <div>
+        <p
+        style="
+        margin: 20px;
+        "
+        >Please contact us if you have any issues, also if you did not request this service please ignore</p>
+        </div>
+
 </div>
-
-<a href="http://localhost:8081/register?client_id={{$client['client']->id}}&client_email={{$client_email}}"
- style="
- background: #0077ff;
- padding: 10px 40px;
- border: none;
- border-radius: 8px;
- text-decoration: none;
- margin: 20px;
- color: white;
- font-weight: bold;
- font-size: 1.25rem;
-"
->Portal Register</a>
-
-<p
- style="
-  margin: 20px;
- "
->Please contact us if you have any issues, also if you did not request this service please ignore</p>
 
 
 <h3 style="margin-bottom: 0;" class="account">{{ $accountName }}</h3>
