b99589fb6990557906fcb533116a90705cc047ce
Thomas <thomasweems3@gmail.com>
2021-03-15 09:07:01 -0500

    started settings controller and migrations

---
+++ app/Http/Controllers/Tenant/SettingsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Tenant;
+
+use Illuminate\Http\Request;
+use App\Models\Tenant\Setting;
+use App\Http\Controllers\Controller;
+
+class SettingsController extends Controller
+{
+    //
+}
